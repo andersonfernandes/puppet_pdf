4280b27a649d317028e6fb55f766e8b2773bb87d
Anderson Fernandes <fernandesanderson14@gmail.com>
2019-07-18 20:26:14 -0300

    Removed old js scripts
    - update pdf_creator calls
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- lib/puppet_pdf.rb
+++ lib/puppet_pdf.rb
@@ -4,14 +4,14 @@ require 'puppet_pdf/railtie' if defined?(Rails)
 
 module PuppetPdf
   def self.pdf_from_url(url, options = {})
-    pdf_creator(:pdf_from_url, url, options).call
+    pdf_creator(:url, url, options).call
   end
 
   def self.pdf_from_html(html, options = {})
-    pdf_creator(:pdf_from_html, html, options).call
+    pdf_creator(:html, html, options).call
   end
 
-  private_class_method def self.pdf_creator(task, source, options)
-    ::PuppetPdf::PdfCreator.new(task, source, options)
+  private_class_method def self.pdf_creator(source_type, source, options)
+    ::PuppetPdf::PdfCreator.new(source_type, source, options)
   end
 end
--- lib/puppet_pdf/pdf_creator.rb
+++ lib/puppet_pdf/pdf_creator.rb
@@ -5,22 +5,22 @@ module PuppetPdf
   class PdfCreator
     include ::Utils::YarnWrapper
 
-    def initialize(task, source, options = {})
+    def initialize(source_type, source, options = {})
       validate_yarn_installation
 
-      @task = task
+      @source_type = source_type
       @source = source
       @output_path = options.fetch(:output_path, default_output_path)
     end
 
     def call
-      run_yarn(task, source, output_path)
+      run_yarn(:create_pdf, source_type, source, output_path)
       output_path
     end
 
     private
 
-    attr_reader :task, :source, :output_path
+    attr_reader :source_type, :source, :output_path
 
     def default_output_path
       file = Tempfile.new(["puppet_pdf_#{Time.now.to_i}", '.pdf'])
