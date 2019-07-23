75013172162e72f3010cc2615f6b9df9c762992b
Anderson Fernandes <fernandesanderson14@gmail.com>
2019-07-23 19:22:07 -0300

    Update render_pdf
    - add specs
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- lib/puppet_pdf/controller_helpers.rb
+++ lib/puppet_pdf/controller_helpers.rb
@@ -1,31 +1,23 @@
 module PuppetPdf
   module ControllerHelpers
     def render_pdf(args = {})
-      args = setup_default_args(args)
-      html = render_to_string(template: args[:template],
-                              layout: args[:layout],
-                              locals: args[:locals])
+      html = render_to_string(template: args.fetch(:template, default_template),
+                              layout: args.fetch(:layout, 'application'),
+                              locals: args.fetch(:locals, {}))
 
       html_path = create_file_and_get_path(html)
-      file_path = ::PuppetPdf::PdfCreator.new(:html, html_path).call
+      options = args.slice(:header, :footer, :margins, :loading_delay)
+      file_path = ::PuppetPdf::PdfCreator.new(html_path, options).call
 
       send_file(file_path,
-                filename: args[:filename],
+                filename: args.fetch(:filename, "#{action_name}.pdf"),
                 disposition: :inline)
     end
 
     private
 
-    def setup_default_args(args = {})
-      args.tap do |a|
-        a[:template]      ||= File.join(controller_path, action_name)
-        a[:layout]        ||= 'application'
-        a[:filename]      ||= "#{action_name}.pdf"
-        a[:header]        ||= '<header></header>'
-        a[:footer]        ||= '<footer></footer>'
-        a[:margins]       ||= { top: 36, right: 36, bottom: 45, left: 36 }
-        a[:loading_delay] ||= 1000
-      end
+    def default_template
+      File.join(controller_path, action_name)
     end
 
     def create_file_and_get_path(content)
--- spec/puppet_pdf/controller_helpers_spec.rb
+++ spec/puppet_pdf/controller_helpers_spec.rb
@@ -1,3 +1,4 @@
+require 'puppet_pdf/pdf_creator'
 require 'puppet_pdf/controller_helpers'
 
 RSpec.describe ::PuppetPdf::ControllerHelpers do
@@ -6,11 +7,42 @@ RSpec.describe ::PuppetPdf::ControllerHelpers do
   describe '#render_pdf' do
     let(:args) do
       {
-
+        template: 'report',
+        layout: 'application_pdf',
+        filename: 'out.pdf',
+        header: '<header></header>',
+        footer: '<footer></footer>',
+        loading_delay: 1000
       }
     end
 
-    before { subject.render_pdf(args) }
+    let(:pdf_creator_class) { double }
+    let(:pdf_creator) { instance_double('PuppetPdf::PdfCreator') }
+    let(:file_path) { '/tmp/out.pdf' }
+
+    before do
+      allow(subject).to receive(:render_to_string).and_return '<html></html>'
+      allow(subject).to receive(:controller_path).and_return 'tasks'
+      allow(subject).to receive(:action_name).and_return 'report'
+      allow(subject).to receive(:send_file).with(kind_of(String), kind_of(Hash))
+
+      stub_const('PuppetPdf::PdfCreator', pdf_creator_class)
+      allow(pdf_creator_class).to receive(:new).and_return pdf_creator
+      allow(pdf_creator).to receive(:call).and_return file_path
+
+      subject.render_pdf(args)
+    end
+
+    it 'should call PdfCreator with html and options' do
+      expected_options = args.slice(:header, :footer, :margins, :loading_delay)
+      expect(pdf_creator_class).to have_received(:new)
+        .with(kind_of(String), expected_options)
+    end
+
+    it 'should call send_file with the file path' do
+      expect(subject).to have_received(:send_file)
+        .with(file_path, disposition: :inline, filename: args[:filename])
+    end
   end
 end
 
