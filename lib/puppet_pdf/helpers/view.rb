dae254d52da3f41a68c2ee5d886ab71bdef53763
Anderson Fernandes <fernandesanderson14@gmail.com>
2019-07-29 22:24:21 -0300

    Add javascript tag and refactor view helpers
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- lib/puppet_pdf/helpers/view.rb
+++ lib/puppet_pdf/helpers/view.rb
@@ -5,9 +5,30 @@ module PuppetPdf
   module Helpers
     module View
       def puppet_stylesheet_link_tag(asset)
+        asset_tag = <<~TAG
+          <style type='text/css'>
+            #{asset_content(asset)}
+          </style>
+        TAG
+
+        asset_tag.html_safe
+      end
+
+      def puppet_javascript_script_tag(asset)
+        asset_tag = <<~TAG
+          <script type='application/javascript'>
+            #{asset_content(asset)}
+          </script>
+        TAG
+
+        asset_tag.html_safe
+      end
+
+      private
+
+      def asset_content(asset)
         url = asset_url(asset)
-        asset_content = OpenURI.open_uri(url, &:read)
-        "<style type='text/css'>#{asset_content}</style>".html_safe
+        OpenURI.open_uri(url, &:read)
       end
     end
   end
--- spec/puppet_pdf/helpers/view_spec.rb
+++ spec/puppet_pdf/helpers/view_spec.rb
@@ -17,8 +17,34 @@ RSpec.describe ::PuppetPdf::Helpers::View do
     end
 
     it 'should return a style tag with the content of the given asset' do
-      expected_tag = "<style type='text/css'>#{asset_content}</style>"
+      expected_tag = <<~TAG
+        <style type='text/css'>
+          #{asset_content}
+        </style>
+      TAG
       expect(subject.puppet_stylesheet_link_tag(asset))
+        .to eq(expected_tag)
+    end
+  end
+
+  describe '#puppet_javascript_script_tag' do
+    let(:asset_content) { "alert('hello')" }
+    let(:asset_url) { 'http://localhost:3000/asset/application.js' }
+    let(:asset) { 'application.js' }
+
+    before do
+      stub_const('OpenURI', open_uri_class)
+      allow(subject).to receive(:asset_url).with(asset).and_return asset_url
+      allow(open_uri_class).to receive(:open_uri).and_return(asset_content)
+    end
+
+    it 'should return a style tag with the content of the given asset' do
+      expected_tag = <<~TAG
+        <script type='application/javascript'>
+          #{asset_content}
+        </script>
+      TAG
+      expect(subject.puppet_javascript_script_tag(asset))
         .to eq(expected_tag)
     end
   end
